f0ba6878313ab22662f1bf8b1a1f8e358331cabc
nematdevelope <nemat.fullstack@gmail.com>
2026-03-25 15:42:43 +0330

    feat: setup express app with user routes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const db = require("./configs/db");
+const userRouter = require("./routes/userRouter");
+
+db();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use("/api/users", userRouter);
+
 app.listen(PORT, () => {
-  console.log(`Server is running in PORT ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
